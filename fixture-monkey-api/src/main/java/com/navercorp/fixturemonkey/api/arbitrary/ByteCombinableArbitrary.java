05744eaa926fa0d39bc1007cc4d0c0e25345392b
kailis <tech.bin.xyz@gmail.com>
2025-07-01 01:38:12 +0900

    byte

---
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/arbitrary/ByteCombinableArbitrary.java
@@ -0,0 +1,112 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.arbitrary;
+
+import java.util.function.Predicate;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+@API(since = "1.1.12", status = Status.EXPERIMENTAL)
+public interface ByteCombinableArbitrary extends CombinableArbitrary<Byte> {
+	@Override
+	Byte combined();
+
+	@Override
+	Byte rawValue();
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces bytes within the specified range.
+	 *
+	 * @param min the minimum value (inclusive)
+	 * @param max the maximum value (inclusive)
+	 * @return the ByteCombinableArbitrary producing bytes between {@code min} and {@code max}
+	 */
+	ByteCombinableArbitrary withRange(byte min, byte max);
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces only positive bytes.
+	 *
+	 * @return the ByteCombinableArbitrary producing positive bytes
+	 */
+	ByteCombinableArbitrary positive();
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces only negative bytes.
+	 *
+	 * @return the ByteCombinableArbitrary producing negative bytes
+	 */
+	ByteCombinableArbitrary negative();
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces only even bytes.
+	 *
+	 * @return the ByteCombinableArbitrary producing even bytes
+	 */
+	ByteCombinableArbitrary even();
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces only odd bytes.
+	 *
+	 * @return the ByteCombinableArbitrary producing odd bytes
+	 */
+	ByteCombinableArbitrary odd();
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces bytes greater than or equal to the specified minimum.
+	 *
+	 * @param min the minimum value (inclusive)
+	 * @return the ByteCombinableArbitrary producing bytes >= {@code min}
+	 */
+	ByteCombinableArbitrary greaterOrEqual(byte min);
+
+	/**
+	 * Generates a ByteCombinableArbitrary which produces bytes less than or equal to the specified maximum.
+	 *
+	 * @param max the maximum value (inclusive)
+	 * @return the ByteCombinableArbitrary producing bytes <= {@code max}
+	 */
+	ByteCombinableArbitrary lessOrEqual(byte max);
+
+	@Override
+	default ByteCombinableArbitrary filter(Predicate<Byte> predicate) {
+		return this.filter(DEFAULT_MAX_TRIES, predicate);
+	}
+
+	@Override
+	default ByteCombinableArbitrary filter(int tries, Predicate<Byte> predicate) {
+		return new ByteCombinableArbitraryDelegator(CombinableArbitrary.super.filter(tries, predicate));
+	}
+
+	@Override
+	default ByteCombinableArbitrary injectNull(double nullProbability) {
+		return new ByteCombinableArbitraryDelegator(CombinableArbitrary.super.injectNull(nullProbability));
+	}
+
+	@Override
+	default ByteCombinableArbitrary unique() {
+		return new ByteCombinableArbitraryDelegator(CombinableArbitrary.super.unique());
+	}
+
+	@Override
+	void clear();
+
+	@Override
+	boolean fixed();
+}
