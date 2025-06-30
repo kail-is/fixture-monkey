49d13dfa9118dd7165eae03349595ee78ebca120
kailis <tech.bin.xyz@gmail.com>
2025-06-30 22:52:40 +0900

    combinable-arbitrary

---
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/arbitrary/CharacterCombinableArbitrary.java
@@ -0,0 +1,105 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.arbitrary;
+
+import java.util.function.Predicate;
+
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
+@API(since = "1.1.12", status = Status.EXPERIMENTAL)
+public interface CharacterCombinableArbitrary extends CombinableArbitrary<Character> {
+	@Override
+	Character combined();
+
+	@Override
+	Character rawValue();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces characters within the specified range.
+	 *
+	 * @param min the minimum character value (inclusive)
+	 * @param max the maximum character value (inclusive)
+	 * @return the CharacterCombinableArbitrary producing characters between {@code min} and {@code max}
+	 */
+	CharacterCombinableArbitrary withRange(char min, char max);
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces only alphabetic characters (a-z, A-Z).
+	 *
+	 * @return the CharacterCombinableArbitrary producing alphabetic characters
+	 */
+	CharacterCombinableArbitrary alpha();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces only numeric characters (0-9).
+	 *
+	 * @return the CharacterCombinableArbitrary producing numeric characters
+	 */
+	CharacterCombinableArbitrary numeric();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces alphanumeric characters (a-z, A-Z, 0-9).
+	 *
+	 * @return the CharacterCombinableArbitrary producing alphanumeric characters
+	 */
+	CharacterCombinableArbitrary alphaNumeric();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces only ASCII printable characters.
+	 *
+	 * @return the CharacterCombinableArbitrary producing ASCII printable characters
+	 */
+	CharacterCombinableArbitrary ascii();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces only uppercase alphabetic characters (A-Z).
+	 *
+	 * @return the CharacterCombinableArbitrary producing uppercase characters
+	 */
+	CharacterCombinableArbitrary uppercase();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces only lowercase alphabetic characters (a-z).
+	 *
+	 * @return the CharacterCombinableArbitrary producing lowercase characters
+	 */
+	CharacterCombinableArbitrary lowercase();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces Korean characters (가-힣).
+	 *
+	 * @return the CharacterCombinableArbitrary producing Korean characters
+	 */
+	CharacterCombinableArbitrary korean();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces emoji characters.
+	 *
+	 * @return the CharacterCombinableArbitrary producing emoji characters
+	 */
+	CharacterCombinableArbitrary emoji();
+
+	/**
+	 * Generates a CharacterCombinableArbitrary which produces whitespace characters.
+	 *
+	 * @return the CharacterCombinableArbitrary producing whitespace characters
+	 */
+	CharacterCombinableArbitrary whitespace();
+}
---
+++ fixture-monkey-api/src/test/java/com/navercorp/fixturemonkey/api/arbitrary/ShortCombinableArbitraryTest.java
@@ -0,0 +1,256 @@
+/*
+ * Fixture Monkey
+ *
+ * Copyright (c) 2021-present NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.fixturemonkey.api.arbitrary;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+import com.navercorp.fixturemonkey.api.exception.FixedValueFilterMissException;
+
+class ShortCombinableArbitraryTest {
+	@Test
+	void combined() {
+		// when
+		Short actual = CombinableArbitrary.shorts().combined();
+
+		// then
+		then(actual).isInstanceOf(Short.class);
+	}
+
+	@Test
+	void withRange() {
+		// given
+		short min = 100;
+		short max = 200;
+
+		// when
+		Short actual = CombinableArbitrary.shorts().withRange(min, max).combined();
+
+		// then
+		then(actual).isBetween(min, max);
+	}
+
+	@Test
+	void positive() {
+		// when
+		boolean allPositive = IntStream.range(0, 100)
+			.mapToObj(i -> CombinableArbitrary.shorts().positive().combined())
+			.allMatch(s -> s > 0);
+
+		// then
+		then(allPositive).isTrue();
+	}
+
+	@Test
+	void negative() {
+		// when
+		boolean allNegative = IntStream.range(0, 100)
+			.mapToObj(i -> CombinableArbitrary.shorts().negative().combined())
+			.allMatch(s -> s < 0);
+
+		// then
+		then(allNegative).isTrue();
+	}
+
+	@Test
+	void even() {
+		// when
+		boolean allEven = IntStream.range(0, 100)
+			.mapToObj(i -> CombinableArbitrary.shorts().even().combined())
+			.allMatch(s -> s % 2 == 0);
+
+		// then
+		then(allEven).isTrue();
+	}
+
+	@Test
+	void odd() {
+		// when
+		boolean allOdd = IntStream.range(0, 100)
+			.mapToObj(i -> CombinableArbitrary.shorts().odd().combined())
+			.allMatch(s -> s % 2 != 0);
+
+		// then
+		then(allOdd).isTrue();
+	}
+
+	@Test
+	void lastMethodWinsWithPositiveAndRange() {
+		// given
+		short min = -100;
+		short max = -50;
+
+		// when - positive()를 무시하고 withRange()가 우선되어야 함
+		Short actual = CombinableArbitrary.shorts().positive().withRange(min, max).combined();
+
+		// then
+		then(actual).isBetween(min, max);
+	}
+
+	@Test
+	void shortUnique() {
+		// when & then - 고정값에 unique() 적용하면 예외 발생해야 함
+		thenThrownBy(() -> CombinableArbitrary.shorts()
+			.filter(x -> x == (short) 42)  // 항상 같은 값만 생성
+			.unique()
+			.combined())
+			.isExactlyInstanceOf(FixedValueFilterMissException.class);
+	}
+
+	@Test
+	void shortMapping() {
+		// when
+		String actual = CombinableArbitrary.shorts()
+			.positive()
+			.map(x -> "short:" + x)
+			.combined();
+
+		// then
+		then(actual).startsWith("short:");
+		String numberPart = actual.substring(6);
+		short value = Short.parseShort(numberPart);
+		then(value).isGreaterThan((short) 0);
+	}
+
+	@Test
+	void shortFiltering() {
+		// when
+		Short actual = CombinableArbitrary.shorts()
+			.withRange((short) 0, (short) 1000)
+			.filter(s -> s > 500)
+			.combined();
+
+		// then
+		then(actual).isGreaterThan((short) 500);
+		then(actual).isLessThanOrEqualTo((short) 1000);
+	}
+
+	@Test
+	void shortFilteringWithMultipleConditions() {
+		// when
+		Short actual = CombinableArbitrary.shorts()
+			.positive()
+			.filter(s -> s % 100 == 0)  // 100의 배수만
+			.combined();
+
+		// then
+		then(actual).isGreaterThan((short) 0);
+		then(actual % 100).isEqualTo(0);
+	}
+
+	@Test
+	void shortInjectNull() {
+		// when
+		Short actual = CombinableArbitrary.shorts()
+			.positive()
+			.injectNull(1.0)  // 100% null
+			.combined();
+
+		// then
+		then(actual).isNull();
+	}
+
+	@Test
+	void shortInjectNullWithZeroProbability() {
+		// when
+		Short actual = CombinableArbitrary.shorts()
+			.positive()
+			.injectNull(0.0)  // 0% null
+			.combined();
+
+		// then
+		then(actual).isNotNull();
+		then(actual).isGreaterThan((short) 0);
+	}
+
+	@Test
+	void shortCombinationWithMultipleOperations() {
+		// when - 여러 연산을 조합
+		String actual = CombinableArbitrary.shorts()
+			.withRange((short) 1, (short) 100)
+			.filter(s -> s % 10 == 0)  // 10의 배수
+			.map(s -> "value:" + s)    // 문자열 변환
+			.combined();
+
+		// then
+		then(actual).startsWith("value:");
+		String numberPart = actual.substring(6);
+		short value = Short.parseShort(numberPart);
+		then(value % 10).isEqualTo(0);
+		then(value).isBetween((short) 1, (short) 100);
+	}
+
+	@Test
+	void shortUniqueWithDifferentValues() {
+		// when - 서로 다른 값들에는 unique()가 정상 작동
+		Short actual = CombinableArbitrary.shorts()
+			.withRange((short) 1, (short) 1000)  // 충분히 넓은 범위
+			.unique()
+			.combined();
+
+		// then
+		then(actual).isBetween((short) 1, (short) 1000);
+	}
+
+	@Test
+	void fixed() {
+		// when
+		boolean actual = CombinableArbitrary.shorts().fixed();
+
+		// then
+		then(actual).isFalse();
+	}
+
+	@Test
+	void lastMethodWinsNegativeOverPositive() {
+		// when - positive()를 무시하고 negative()가 우선되어야 함
+		boolean allNegative = IntStream.range(0, 30)
+			.mapToObj(i -> CombinableArbitrary.shorts().positive().negative().combined())
+			.allMatch(s -> s < 0);
+
+		// then
+		then(allNegative).isTrue();
+	}
+
+	@Test
+	void lastMethodWinsEvenOverRange() {
+		// when - withRange()를 무시하고 even()이 우선되어야 함
+		boolean allEven = IntStream.range(0, 30)
+			.mapToObj(i -> CombinableArbitrary.shorts().withRange((short) 1, (short) 1000).even().combined())
+			.allMatch(s -> s % 2 == 0);
+
+		// then
+		then(allEven).isTrue();
+	}
+
+	@Test
+	void shortFilterWithHundredMultiple() {
+		// when - 100의 배수만 필터링
+		boolean allMultipleOfHundred = IntStream.range(0, 30)
+			.mapToObj(i -> CombinableArbitrary.shorts().withRange((short) 0, (short) 1000).filter(s -> s % 100 == 0).combined())
+			.allMatch(s -> s % 100 == 0);
+
+		// then
+		then(allMultipleOfHundred).isTrue();
+	}
+}
